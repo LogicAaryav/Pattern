7bc7b7295092a11dbc29a9cc4f1526df562019c8
LogicAaryav <132733460+LogicAaryav@users.noreply.github.com>
2023-05-07 11:54:47 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 basic.forever(function () {
     for (let index = 0; index < 2; index++) {
         pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(1000)
+        basic.pause(200)
         pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(200)
     }
     pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(1000)
+    basic.pause(200)
     pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(200)
     for (let index = 0; index < 2; index++) {
         pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(1000)
+        basic.pause(200)
         pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(200)
     }
 })
